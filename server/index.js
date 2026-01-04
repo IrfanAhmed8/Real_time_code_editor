4a070be77960a533a1c9f21f1fddd6fcd262058f
IrfanAhmed8 <jafriirfan37@gmail.com>
2026-01-04 14:34:15 +0530

    added the frontend url

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "*",
+    origin: "https://real-time-code-editor-dusky.vercel.app/",
     methods: ["GET", "POST"],
   },
 });
